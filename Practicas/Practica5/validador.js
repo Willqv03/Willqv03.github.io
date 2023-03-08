df13f770b49a5718559b9931a705245ae329ab9b
Willqv03 <125417208+Willqv03@users.noreply.github.com>
2023-03-08 16:07:42 -0400

    Add files via upload

---
+++ Practicas/Practica5/validador.js
@@ -0,0 +1,53 @@
+
+const entrada = document.querySelector("#ced");
+let result = document.getElementById("salidaResultado");
+entrada.addEventListener("keyup",borrar);
+
+function borrar(){
+  result.innerText = " ";
+}
+function comprobar(cedula){
+  console.log(cedula.length);
+  let valores = cedula.slice(0,3);
+  if(valores === "000"){
+    return false;
+  }
+  if(cedula.length < 11){
+    return false;
+  }
+  return true;
+}
+
+function validar() {
+  let cedula = entrada.value;
+  let cedArray = cedula.split("");
+  let condicion = comprobar(cedula);
+  let digitoVerificador = parseInt(cedArray[cedArray.length - 1]);
+  let sumaIndiceImpar = 0;
+  let grupoNumerosIndicePar = "";
+  let sumaIndicePar = 0;
+  let resultado = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let indiceImpar = 2 * i;
+    let indicePar = 2 * i + 1;
+    sumaIndiceImpar += parseInt(cedArray[indiceImpar]);
+    digitoIndicePar = parseInt(cedArray[indicePar]) * 2;
+    grupoNumerosIndicePar += digitoIndicePar;
+  }
+
+  grupoNumerosIndicePar = grupoNumerosIndicePar.split("");
+
+  for (let i = 0; i < grupoNumerosIndicePar.length; i++) {
+    sumaIndicePar += parseInt(grupoNumerosIndicePar[i]);
+  }
+
+  resultado = ((sumaIndiceImpar + sumaIndicePar) * 9) % 10;
+
+  if (resultado == digitoVerificador && condicion) {
+    result.innerText = "La cedula ingresada es valida :)";
+  } else {
+    result.innerText = "La cedula ingresada es invalida";
+  }
+}
+
