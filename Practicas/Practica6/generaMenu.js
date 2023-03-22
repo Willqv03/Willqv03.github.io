eee8878ce2f259978502cfdfcab3e0e1ce16d36f
Willqv03 <willyqv405@gmail.com>
2023-03-22 15:55:48 -0400

    22MAR

--- Practicas/Practica6/generaMenu.js
+++ Practicas/Practica6/generaMenu.js
@@ -1,7 +1,30 @@
+var menu = document.getElementById("nav");
 fetch("./datosMenu.json")
 .then (function(resp){
     return resp.json();
 })
-.then(function(data){
-    console.log(data);
+.then(function(datos){
+    console.log(datos);
+
+    generarMenu(datos,menu,"menu-horizontal");
+
 });
+
+function generarMenu(datos,menu,id){
+    const lista = document.createElement('ul'); 
+    lista.setAttribute("id",id);
+    menu.appendChild(lista);
+  
+    for (let i = 0; i < datos.length; i++) {
+        const registro = document.createElement('li');
+        const enlace = document.createElement('a');
+        enlace.setAttribute('href', datos[i].link);
+        enlace.textContent = datos[i].nombre;
+        registro.appendChild(enlace);
+  
+        if (datos[i].submenu != null) {
+            generarMenu(datos[i].submenu,registro,null);
+        }
+        lista.appendChild(registro);
+    }
+}
