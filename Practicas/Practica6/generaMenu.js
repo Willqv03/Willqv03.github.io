6a535312547b29dfba01a6dcaa3c1b8467a9567f
Willqv03 <willyqv405@gmail.com>
2023-03-29 12:42:05 -0400

    modificacion

--- Practicas/Practica6/generaMenu.js
+++ Practicas/Practica6/generaMenu.js
@@ -1,30 +1,64 @@
-var menu = document.getElementById("nav");
+var menu = document.getElementById("menu");
 fetch("./datosMenu.json")
 .then (function(resp){
     return resp.json();
 })
 .then(function(datos){
-    console.log(datos);
 
-    generarMenu(datos,menu,"menu-horizontal");
+    generarMenu(datos,menu);
 
 });
 
-function generarMenu(datos,menu,id){
-    const lista = document.createElement('ul'); 
-    lista.setAttribute("id",id);
+function generarMenu(datos,menu){
+    let json = datos;
+    const lista = document.createElement('ul');
+    lista.setAttribute("class","menu");
     menu.appendChild(lista);
   
-    for (let i = 0; i < datos.length; i++) {
-        const registro = document.createElement('li');
-        const enlace = document.createElement('a');
-        enlace.setAttribute('href', datos[i].link);
-        enlace.textContent = datos[i].nombre;
-        registro.appendChild(enlace);
-  
-        if (datos[i].submenu != null) {
-            generarMenu(datos[i].submenu,registro,null);
+    for (let i = 0; i < json.length; i++) {
+        if(datos[i].padre == null){
+            const registro = document.createElement('li');
+            const enlace = document.createElement('a');
+            enlace.setAttribute('href', datos[i].link);
+            enlace.textContent = datos[i].nombre;
+            registro.appendChild(enlace);
+            let prueba = 0;
+            let ul = document.createElement('ul');
+            for (let pos = 0; pos < json.length; pos++) {
+                if (datos[i].id == json[pos].padre) { 
+                    prueba++;
+                    if(prueba == 1){
+                        registro.appendChild(ul); 
+                        ul.appendChild(generarSubmenu(json[pos],ul, json,"submenu"));   
+                    }else{
+                        ul.appendChild(generarSubmenu(json[pos],ul, json,"submenu")); 
+                    }
+                }
+            }
+            lista.appendChild(registro);
         }
-        lista.appendChild(registro);
     }
+}
+
+function generarSubmenu(submenu,menu,json,clase){
+    menu.setAttribute("class",clase)
+    const registro = document.createElement('li');
+    const enlace = document.createElement('a');
+    enlace.setAttribute('href', submenu.link);
+    enlace.textContent = submenu.nombre;
+    registro.appendChild(enlace);
+    let prueba = 0;
+    let ul = document.createElement('ul');;
+    for (let pos = 0; pos < json.length; pos++) {
+        if (submenu.id == json[pos].padre) { 
+            prueba++;
+            if(prueba == 1){
+                registro.appendChild(ul); 
+                ul.appendChild(generarSubmenu(json[pos],ul, json,"submenu2"));   
+            }else{
+                ul.appendChild(generarSubmenu(json[pos],ul, json,"submenu2")); 
+            }
+        }
+    } 
+    return registro;
 }
